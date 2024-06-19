69fe4947bbe70c497a95e3183ece954b5497e454
Omkar Jethe <98147636+omkarsjethe@users.noreply.github.com>
2024-06-19 14:47:27 +0530

    Add files via upload

---
+++ Sql_Order.sql
@@ -0,0 +1,18 @@
+ show databases ;
+ 
+ use omkardb;
+ 
+ select * from employee ;
+ 
+ select * from employee
+ order by empage;
+ 
+ select * from employee order by  empage desc;
+ 
+ select * from employee order by empname ;
+ 
+ select * from employee order by empname desc ;
+  
+select * from employee order by empage,empname ;
+
+select * from employee order by empcity desc ,empage  asc;

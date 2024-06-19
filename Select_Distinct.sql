fdf1ca4999aa083e688c207d853de05e0cbb9751
Omkar Jethe <98147636+omkarsjethe@users.noreply.github.com>
2024-06-19 14:07:24 +0530

    Add files via upload

---
+++ Select_Distinct.sql
@@ -0,0 +1,13 @@
+show databases;
+
+use omkardb;
+
+Select * from subject;
+
+insert into subject values (5,"C++",95);
+
+select distinct sname from subject ;
+
+select count(distinct sname ="Maths")from subject;
+)
+select distinct sname = "Maths" from subject;

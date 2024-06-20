7694220724aa0a1778c3e951021992b2dc80bcc4
Omkar Jethe <98147636+omkarsjethe@users.noreply.github.com>
2024-06-20 16:07:35 +0530

    Add files via upload

---
+++ Sql_MinMax().sql
@@ -0,0 +1,27 @@
+show databases;
+
+use omkardb;
+
+select * from students;
+
+Select min(marks) from Students;
+
+Select min(marks) from Students;
+
+Select min(fail_subjects) from students;
+
+Select min(fail_subjects) from students
+where city ="Pune";
+
+select min(marks) as minimummarks;
+ 
+ select max(marks) from students;
+ 
+ select max(marks) from students
+ 
+ 
+ where city ="Hyderabad"
+ group by fail_subjects;
+ 
+
+from students;

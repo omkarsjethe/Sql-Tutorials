dc0e86cf236994f7c7c702f248b5d54cce16b6b6
Omkar Jethe <98147636+omkarsjethe@users.noreply.github.com>
2024-07-02 18:05:34 +0530

    Add files via upload

---
+++ Sql_copy.sql
@@ -0,0 +1,6 @@
+show databases;
+ use omkardb;
+ 
+ create table students like student;
+ 
+truncate students;
---
+++ Sql_renametable.sql
@@ -0,0 +1,8 @@
+show databases;
+ use omkardb;
+
+select * from students;
+
+RENAME TABLE students TO Student;
+
+
